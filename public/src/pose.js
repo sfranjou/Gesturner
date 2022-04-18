fcfd5b4b70bf50d880894aae27b393952a9d9c38
Sebastian Franjou <sfranjou@mit.edu>
2022-04-18 15:12:41 -0400

    can draw transparent rectangle over video feed that resizes with video

--- public/src/pose.js
+++ public/src/pose.js
@@ -55,7 +55,46 @@ const handsfree = new Handsfree({
 })
 handsfree.enablePlugins('browser')
 
+let drawActiveZone = () =>
+{
+    const canvas = document.getElementById("debugger-canvas") // first make a index . html with a canvas height and width and id of pong or whatever
+    // and then we have to get access to our context to draw on it
+    const context = canvas.getContext('2d')
 
+    // then you do...
+
+    // context.fillStyle = 'green';
+    // https://stackoverflow.com/questions/442404/retrieve-the-position-x-y-of-an-html-element
+    let el = document.querySelector('#debugger-holder');
+    // let videoBounds = document.querySelector('#debugger-holder').getBoundingClientRect();
+    // let el = document.getElementsByClassName('handsfree-canvas')[0];
+    // console.log(el)
+    // let videoBounds = el.getBoundingClientRect();
+    // let x = videoBounds.x
+    // let y = videoBounds.y
+    // let width = videoBounds.width
+    // let width = context.canvas.clientWidth
+    let width = el.offsetWidth
+    // let height = videoBounds.height
+    // let height = context.canvas.clientHeight
+    let height = el.offsetHeight
+    // console.log(x, y, width, height)
+    console.log(width, height)
+    // TODO: the width and hieght don't change even though the video changes size, not sure 
+
+    // source for debug eleme4nt code https://github.com/MIDIBlocks/handsfree/blob/481f7cdba849940e1472a370e7ab7418487629a8/src/handsfree.js#L840  
+
+    console.log(height, width, canvas.style.height, canvas.style.width)
+    canvas.style.height = height.toString() + "px";
+    canvas.style.width = width.toString() + "px";
+    console.log(height, width, canvas.style.height, canvas.style.width)
+    context.fillStyle = 'rgba(225,0,0,0.5)';
+    // context.fillStyle = 'green';
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    // context.fillRect(0, 0, width, height);
+    context.fillRect(0, 0, canvas.width * 0.3, canvas.height * 0.2);
+    // context.fillRect(10, 10, 250, 250);
+}
 
 // reeference for plugins: https://handsfree.js.org/guide/the-loop.html#basic-plugins
 // A plugin that console logs your data on every frame
@@ -91,6 +130,8 @@ handsfree.use('UIupdater', (data) =>
     document.getElementById("right-hand-x").textContent = "Right Hand x: " + data.pose.poseLandmarks[rightHandIdx].x;
     document.getElementById("right-hand-y").textContent = "Right Hand y: " + data.pose.poseLandmarks[rightHandIdx].y;
     document.getElementById("active-indicator").textContent = "Is Active: " + isActive(data);
+
+    drawActiveZone();
 })
 
 // handsfree.plugin.consoleLogger.enable()
